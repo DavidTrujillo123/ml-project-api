10e5572caede7c05b085f7850bb5c43d281e9c76
CRISTIAN JEFFERSON CASTRO GOMEZ <cjcastrog@utn.edu.ec>
2024-07-15 12:33:16 -0500

    Add Template FORMS on the file main

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import joblib
 import numpy as np
 from flask_cors import CORS
@@ -14,31 +14,32 @@ CORS(app)
 # Cargar el modelo de regresión logística
 log_reg = joblib.load('api/models/logistic_regression_model.pkl')
 
-# # Cargar el modelo de red neuronal
+# Cargar el modelo de red neuronal
 model_sigmoid = load_model('api/models/model_sigmoid.h5')
 # model_relu = load_model('api/models/model_relu.h5')
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'API de predicción de Diabetes'
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json(force=True)
+    data = request.form.to_dict()
+    data = {key: float(value) for key, value in data.items()}
 
     columns = ['gender', 'age', 'hypertension', 'heart_disease', 'smoking_history', 'bmi', 'HbA1c_level', 'blood_glucose_level']
-    new_data = pd.DataFrame([data['input']], columns=columns)
+    new_data = pd.DataFrame([data], columns=columns)
 
     # Estandarizar los nuevos datos utilizando el scaler ajustado
     new_data_scaled = scaler.transform(new_data)
 
-#     # Predicción con el modelo de regresión logística
+    # Predicción con el modelo de regresión logística
     log_reg_prediction = log_reg.predict(new_data_scaled)
 
-#     # Predicción con el modelo de red neuronal (sigmoide)
+    # Predicción con el modelo de red neuronal (sigmoide)
     model_sigmoid_prediction = model_sigmoid.predict(new_data_scaled)
 
-#     # Predicción con el modelo de red neuronal (relu)
+    # Predicción con el modelo de red neuronal (relu)
     # model_relu_prediction = model_relu.predict(new_data_scaled)
 
     return jsonify({
@@ -46,7 +47,6 @@ def predict():
         'model_sigmoid_prediction': model_sigmoid_prediction.tolist()[0][0],
         # 'model_relu_prediction': model_relu_prediction.tolist()[0][0],
         'coeficientes_regresion_log': log_reg.coef_.tolist()[0]
-
     })
 
 if __name__ == '__main__':
